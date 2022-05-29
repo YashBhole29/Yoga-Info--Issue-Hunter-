3b75cab6e7604ffa0040454bb144f6ed2ad022b4
YashBhole29 <105486311+YashBhole29@users.noreply.github.com>
2022-05-29 20:16:22 +0530

    Yoga App

---
+++ app/src/main/java/com/example/yogainfo/MainActivity2.kt
@@ -0,0 +1,53 @@
+package com.example.yogainfo
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val button1:ImageButton = findViewById(R.id.imageButton1)
+        button1.setOnClickListener {
+            active2()
+        }
+
+        val button2:ImageButton = findViewById(R.id.imageButton2)
+        button2.setOnClickListener {
+            active3()}
+
+        val button3:ImageButton = findViewById(R.id.imageButton3)
+        button3.setOnClickListener {
+            active4()}
+
+        val button4:ImageButton = findViewById(R.id.imageButton4)
+        button4.setOnClickListener {
+            active5()}
+    }
+
+    private fun active2() {
+        val intent = Intent(this,MainActivity2::class.java)
+        startActivity(intent)
+    }
+
+    private fun active3() {
+        val intent = Intent(this,MainActivity3::class.java)
+        startActivity(intent)
+    }
+
+    private fun active4() {
+        val intent = Intent(this, MainActivity4::class.java)
+        startActivity(intent)
+    }
+
+    private fun active5() {
+        val intent = Intent(this, MainActivity5::class.java)
+        startActivity(intent)
+    }
+
+}
---
+++ app/src/main/java/com/example/yogainfo/MainActivity5.kt
@@ -0,0 +1,50 @@
+package com.example.yogainfo
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main5)
+
+        val button1:ImageButton = findViewById(R.id.imageButton1)
+        button1.setOnClickListener {
+            active2()
+        }
+
+        val button2:ImageButton = findViewById(R.id.imageButton2)
+        button2.setOnClickListener {
+            active3()}
+
+        val button3:ImageButton = findViewById(R.id.imageButton3)
+        button3.setOnClickListener {
+            active4()}
+
+        val button4:ImageButton = findViewById(R.id.imageButton4)
+        button4.setOnClickListener {
+            active5()}
+    }
+
+    private fun active2() {
+        val intent = Intent(this,MainActivity2::class.java)
+        startActivity(intent)
+    }
+
+    private fun active3() {
+        val intent = Intent(this,MainActivity3::class.java)
+        startActivity(intent)
+    }
+
+    private fun active4() {
+        val intent = Intent(this, MainActivity4::class.java)
+        startActivity(intent)
+    }
+
+    private fun active5() {
+        val intent = Intent(this, MainActivity5::class.java)
+        startActivity(intent)
+    }
+}
